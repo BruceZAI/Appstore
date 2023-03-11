5844c1601cee3e2d139bf97ce172d19328a76d52
“BruceZAI” <changjun.zai@outlook.com>
2023-03-11 15:47:24 -0800

    first commit

---
+++ appstore/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+const (
+    APP_INDEX = "app"
+    USER_INDEX = "user"
+    ES_URL = "http://34.102.43.194:9200"
+    ES_USERNAME = "brucezai97"
+    ES_PASSWORD = "z19971015"
+    STRIPE_API_KEY = "sk_test_51Me7saItWWxXiaepA4Y95khCCusHqQhKXIo8SfgGNl9c4YmohfPXiab4ZGvEKYy13TXmYN0CeZqSqfELnlZjaDK800m3c9vDJj"
+    GCS_BUCKET = "brucezai-appstore"
+)
